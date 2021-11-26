94ca1c00f9997aaeb1e297d1b53d9e21f31966bf
ZhengJiaBin1208 <zhengjiabin@bestseller.com.cn>
2021-11-26 15:14:50 +0800

    1.两数之和题解 2.两数相加题解 3.无重复字符的最长子串题解

--- leetcode/editor/cn/[3]无重复字符的最长子串.java
+++ leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -49,7 +49,19 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        int []hash = new int [500];
+        int max = 0;
+        int i = 0, j = 0;
+        while (i < s.length() && j <s.length() )
+        {
+            if(hash[s.charAt(j)] == 0)
+            { hash[s.charAt(j)] = 1;
+                j++;
+                max = (j - i) > max ? (j - i) : max;
+            } else
+            { hash[s.charAt(i)] = 0; i++; }
+        }
+        return max;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
